a69969c2600abd784af1b2772d7944175c72cf37
William Castellano <williamcastellano@Williams-MacBook-Pro.local>
2017-06-17 22:47:39 -0700

    added splash page

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ var db = require('./db');
 var UserController = require('./user/UserController');
 app.use('/users', UserController);
 
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
 module.exports = app;
