64cb6f059b571ed2ffce68e6d2ccccb37672f5a6
William Castellano <williamcastellano@Williams-MacBook-Pro.local>
2017-06-17 23:43:48 -0700

    added more examples

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // app.js
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 var db = require('./db');
@@ -9,8 +10,21 @@ app.use('/users', UserController);
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.get('/times', function(request, response) {
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i=0; i < times; i++)
+      result += i + ' ';
+  response.send(result);
 });
 
 module.exports = app;
